edfcd9c31843a21038f541b4f901ed230d0cc3b4
penumalligowripravallika <penumalligowripravallika09@gmail.com>
2026-07-15 14:01:40 +0530

    Add files via upload

---
+++ Abstraction/com/wipro/automobile/ship/TestCompartment.java
@@ -0,0 +1,11 @@
+package com.wipro.automobile.ship;
+
+public class TestCompartment {
+	 public static void main(String[] args) {
+
+	        Compartment c = new Compartment(10, 20, 30);
+
+	        c.display();
+	    }
+
+}
