591a77ee7909249e4db6909dcd7c146fdf7339c5
penumalligowripravallika <penumalligowripravallika09@gmail.com>
2026-07-22 13:57:18 +0530

    Add files via upload

---
+++ MultiThreading/threadcontrol/EvenOddThread.java
@@ -0,0 +1,39 @@
+package threadcontrol;
+class EvenThread extends Thread 
+{
+    public void run() 
+    {
+        for (int i = 2; i <= 20; i += 2) 
+        {
+            System.out.println(i);
+        }
+    }
+}
+class OddThread extends Thread 
+{
+    public void run() 
+    {
+        for (int i = 1; i <= 20; i += 2) 
+        {
+            System.out.println(i);
+        }
+    }
+}
+public class EvenOddThread 
+{
+    public static void main(String[] args) 
+    {
+        EvenThread even = new EvenThread();
+        OddThread odd = new OddThread();
+        even.start();
+        try 
+        {
+            even.join();
+        } 
+        catch (InterruptedException e) 
+        {
+            e.printStackTrace();
+        }
+        odd.start();
+    }
+}
